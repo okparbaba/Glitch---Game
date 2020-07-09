a65d7ed2a8596b422b81369b43960334eff4e218
yannaing-lynn <rghtbossy@gmail.com>
2020-07-09 14:40:40 +0630

    first

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacker attackerPrefab;
+    bool spawn = true;
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab,transform.position,transform.rotation);
+    }
+}
